cdccb9348155a185002779d68ad0dd86a8d060b6
gdolphin <gdolphin@mit.edu>
2020-05-29 14:51:36 -0500

    string pulling works

--- Assets/Scripts/Navigation/NavigationMesh.cs
+++ Assets/Scripts/Navigation/NavigationMesh.cs
@@ -65,6 +65,7 @@ public class Graph<T> where T : class, INode
             }
             curNode = frontier.ExtractMin();
         }
+
         return TraceBackPointers(backPointers, end);
     }
 
@@ -77,6 +78,7 @@ public class Graph<T> where T : class, INode
             path.Add(curNode);
             curNode = backPointers[curNode];
         }
+        path.Reverse();
         return path;
     }
 
@@ -164,47 +166,6 @@ public class NavigationMesh : MonoBehaviour
         }
 
         List<Vector2> edgePortals = new List<Vector2>();
-        /*
-        Vector2Int e = triPairToEdgeMap[new Vector2Int(triPath[0], triPath[1])];
-        Vector2 v1 = mesh.vertices[e[0]];
-        Vector2 v2 = mesh.vertices[e[1]];
-
-        if (VecMath.Det(v1 - startPos, v2 - startPos) >= 0)
-        {
-            //counter clockwise rotation
-            edgePortals.Add(v1);
-            edgePortals.Add(v2);
-        }
-        else
-        {
-            //clockwise rotation
-            edgePortals.Add(v2);
-            edgePortals.Add(v1);
-        }
-
-        //Make sure that all edge portals are oriented in the same way
-        for (int i = 2; i < triPath.Count; i++)
-        {
-            int tri1 = triPath[i - 1];
-            int tri2 = triPath[i];
-            e = triPairToEdgeMap[new Vector2Int(tri1, tri2)];
-            v1 = mesh.vertices[e[0]];
-            v2 = mesh.vertices[e[1]];
-
-            if (v1 == edgePortals[edgePortals.Count - 2] ||
-                    v2 == edgePortals[edgePortals.Count - 1])
-            {
-                edgePortals.Add(v1);
-                edgePortals.Add(v2);
-            }
-            else
-            {
-                edgePortals.Add(v2);
-                edgePortals.Add(v1);
-            }
-        }
-        */
-
         for(int i = 0; i < triPath.Count - 1; i++)
         {
             Triangle tri = triPath[i];
@@ -225,14 +186,14 @@ public class NavigationMesh : MonoBehaviour
         edgePortals.Add(targetPos);
         edgePortals.Add(targetPos);
 
-        
+        /*
         for(int i = 0; i < edgePortals.Count; i+=2)
         {
-            //Debug.DrawLine(new Vector2(10.0f, 0.0f), transform.TransformPoint(edgePortals[i]), Color.cyan, 0.0f, false);
+            //Debug.DrawLine(startPos, transform.TransformPoint(edgePortals[i]), Color.cyan, 0.0f, false);
             Debug.DrawLine(transform.TransformPoint(edgePortals[i]),
                 transform.TransformPoint(edgePortals[i + 1]), Color.magenta, 0.0f, false);
         }
-        
+        */
 
         //Run Simple Stupid Funnel Algorithm.
         List<Vector2> breadCrumbs = new List<Vector2> { startPos };
@@ -307,139 +268,6 @@ public class NavigationMesh : MonoBehaviour
 
     }
 
-    /*
-    List<Vector2> StringPullingAlgorithm(List<int> triPath, Vector2 startPos, Vector2 targetPos)
-    {
-        if (triPath.Count == 1)
-        {
-            return new List<Vector2> { startPos, targetPos };
-        }
-
-        List<Vector2> edgePortals = new List<Vector2>();
-        Vector2Int e = triPairToEdgeMap[new Vector2Int(triPath[0], triPath[1])];
-        Vector2 v1 = mesh.vertices[e[0]];
-        Vector2 v2 = mesh.vertices[e[1]];
-        
-        if (VecMath.Det(v1 - startPos, v2 - startPos) >= 0)
-        {
-            //counter clockwise rotation
-            edgePortals.Add(v1);
-            edgePortals.Add(v2);
-        }
-        else
-        {
-            //clockwise rotation
-            edgePortals.Add(v2);
-            edgePortals.Add(v1);
-        }
-
-        //Make sure that all edge portals are oriented in the same way
-        for (int i = 2; i < triPath.Count; i++)
-        {
-            int tri1 = triPath[i - 1];
-            int tri2 = triPath[i];
-            e = triPairToEdgeMap[new Vector2Int(tri1, tri2)];
-            v1 = mesh.vertices[e[0]];
-            v2 = mesh.vertices[e[1]];
-
-            if(v1 == edgePortals[edgePortals.Count - 2] || 
-                    v2 == edgePortals[edgePortals.Count - 1])
-            {
-                edgePortals.Add(v1);
-                edgePortals.Add(v2);
-            }
-            else
-            {
-                edgePortals.Add(v2);
-                edgePortals.Add(v1);
-            }
-        }
-
-        edgePortals.Add(targetPos);
-        edgePortals.Add(targetPos);
-
-        //for(int i = 0; i < edgePortals.Count; i+=2)
-        //{
-        //    Debug.DrawLine(transform.TransformPoint(edgePortals[i]),
-        //        transform.TransformPoint(edgePortals[i + 1]), Color.magenta, 0.0f, false);
-        //}
-        
-        //Run Simple Stupid Funnel Algorithm.
-        List<Vector2> breadCrumbs = new List<Vector2> { startPos };
-        Vector2 funnelL = edgePortals[0] - breadCrumbs[breadCrumbs.Count - 1];
-        Vector2 funnelR = edgePortals[1] - breadCrumbs[breadCrumbs.Count - 1];
-
-        //Left funnel refers to left from our point of view looking down on navmesh
-
-        int t = 0;
-        int epL = 2;
-        int epR = 2;
-        int ep = 2;
-
-        while (ep < edgePortals.Count && t < 1000)
-        {
-            Vector2 newFunnelL = edgePortals[ep] - breadCrumbs[breadCrumbs.Count - 1];
-            if (VecMath.Det(funnelL, newFunnelL) >= 0)
-            {
-                if (edgePortals[ep] == edgePortals[epL] || VecMath.Det(newFunnelL, funnelR) >= 0)
-                {
-                    funnelL = newFunnelL;
-                    epL = ep;
-                }
-                else
-                {
-                    breadCrumbs.Add(breadCrumbs[breadCrumbs.Count - 1] + funnelR);
-                    ep = epR;
-                    epL = ep;
-                    epR = ep;
-                    funnelL = edgePortals[epL] - breadCrumbs[breadCrumbs.Count - 1];
-                    funnelR = edgePortals[epR + 1] - breadCrumbs[breadCrumbs.Count - 1];
-                    continue;
-                }
-
-            }
-
-            Vector2 newFunnelR = edgePortals[ep + 1] - breadCrumbs[breadCrumbs.Count - 1];
-            if (VecMath.Det(funnelR, newFunnelR) <= 0)
-            {
-                if (edgePortals[ep + 1] == edgePortals[epR + 1] || VecMath.Det(funnelL, newFunnelR) >= 0)
-                {
-                    funnelR = newFunnelR;
-                    epR = ep;
-                }
-                else
-                {
-                    breadCrumbs.Add(breadCrumbs[breadCrumbs.Count - 1] + funnelL);
-                    ep = epL;
-                    epL = ep;
-                    epR = ep;
-                    funnelL = edgePortals[epL] - breadCrumbs[breadCrumbs.Count - 1];
-                    funnelR = edgePortals[epR + 1] - breadCrumbs[breadCrumbs.Count - 1];
-                    continue;
-                }
-            }
-
-            ep += 2;
-            t += 1;
-        }
-
-        if (t == 100)
-        {
-            Debug.LogError("FUNNEL ALG FAILED");
-        }
-
-        breadCrumbs.Add(targetPos);
-        for (int i = 1; i < breadCrumbs.Count; i++)
-        {
-            Debug.DrawLine(transform.TransformPoint(breadCrumbs[i - 1]),
-                               transform.TransformPoint(breadCrumbs[i]), Color.green, 0.0f, false);
-        }
-        
-        return breadCrumbs;
-        
-    }
-    */
-
     public Vector2[] GetShortestPathFromTo(Vector2 start, Vector2 destination)
     {
         Vector2 startPosLocal = transform.InverseTransformPoint(start);
@@ -448,18 +276,6 @@ public class NavigationMesh : MonoBehaviour
         Triangle endTri = FindContainingTriangle(endPosLocal);
 
         List<Triangle> triPath = navMeshGraph.DijkstrasAlgorithm(startTri, endTri);
-
-        /*
-        for (int i = 0; i < triPath.Count - 1; i++)
-        {
-            Triangle curTri = triPath[i];
-            Triangle nextTri = triPath[i + 1];
-            Vector2 c1 = (curTri.edge.origin.p + curTri.edge.next.origin.p + curTri.edge.next.next.origin.p) / 3;
-            Vector2 c2 = (nextTri.edge.origin.p + nextTri.edge.next.origin.p + nextTri.edge.next.next.origin.p) / 3;
-            Debug.DrawLine(c1, c2, Color.magenta, 0.0f, false);
-        }
-        */
-
         List<Vector2> shortestPathLocal = StringPullingAlgorithm(triPath, startPosLocal, endPosLocal);
 
         Vector2[] shortestPathWorld = new Vector2[shortestPathLocal.Count - 1];
@@ -474,7 +290,6 @@ public class NavigationMesh : MonoBehaviour
     private Triangle FindContainingTriangle(Vector2 p)
     {
         return mesh.FindContainingTriangle(p);
-
     }
     
 }
