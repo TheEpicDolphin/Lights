6f985124980af77cd5748f994ed247dc9c6e9f8c
gdolphin <gdolphin@mit.edu>
2020-07-12 13:11:54 -0700

    saving

--- Assets/Scripts/AI/UtilityAI.cs
+++ Assets/Scripts/AI/UtilityAI.cs
@@ -1,45 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Xml.Serialization;
 
-/*
- * Consider having multiple layers such as (Movement layer, LookAt layer, Attack layer, etc)
- * In each layer, actions are selected based on utility and each layer is independent from the rest.
- * There is also possibly a Script layer that takes over all of them
- * 
- */
+
 
 public class UtilityAI
 {
-    public Dictionary<string, object> utilityMemory;
-    private List<UtilityBucket> utilityBuckets;
-    UtilityAction currentAction;
+    public Dictionary<string, UtilityBucket> utilityMemory;
+    [XmlElement("actions")]
+    public List<UtilityAction> actions;
 
-    public UtilityAI(List<UtilityBucket> utilityBuckets)
+    public UtilityAI CreateFromXLM()
     {
-        this.utilityMemory = new Dictionary<string, object>();
-        this.utilityBuckets = utilityBuckets;
-        this.currentAction = new Wait(0.0f);
+        Hero hero = XMLOp.Deserialize<Hero>("hero.xml");
+        Debug.Log(hero.name);
     }
-
-    public void RunOptimalAction()
-    {
-        UtilityBucket optimalBucket = new UtilityBucket("default");
-        float bestScore = 0.0f;
-        foreach (UtilityBucket bucket in utilityBuckets)
-        {
-            float score = bucket.EvaluatePriority(utilityMemory);
-            if (score > bestScore)
-            {
-                bestScore = score;
-                optimalBucket = bucket;
-            }
-        }
-        optimalBucket.Run(utilityMemory);
-    }
-
-    public void AddMemory(string key, object obj)
-    {
-        utilityMemory[key] = obj;
-    }
+    
 }
